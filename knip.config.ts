5c071a7b731661703be71ff550e94a762b0c8ab3
Antony Rizzitelli <rizzitelli.antony@pm.me>
2026-09-24 07:54:08 +0200

    chore(deps): update antelope dependencies to latest

--- knip.config.ts
+++ knip.config.ts
@@ -3,5 +3,4 @@ import { antelopeKnipConfig } from "@antelopejs/tooling-configs/knip";
 export default antelopeKnipConfig({
   entry: ["test/*.test.js", "src/tests/**/*.test.ts", "src/antelope.test.ts"],
   project: ["test/*.test.js"],
-  ignoreBinaries: ["ajs"],
 });
